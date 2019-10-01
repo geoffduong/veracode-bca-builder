bc50f38e073b2854a0df875362a53651498c9129
Geoff Duong <duongg4848@gmail.com>
2019-10-01 14:25:59 -0400

    Added param for building a Release configuration

--- veracode-bca-builder.sh
+++ veracode-bca-builder.sh
@@ -4,6 +4,7 @@
 PROJECT_PATH="$1"
 SCHEME=$2
 OUTPUT_LOCATION="$3"
+IS_RELEASE="$4"
 
 # DETERME THE XCODE TYPE
 if [ ${PROJECT_PATH:(-9)} = "workspace" ]
@@ -21,7 +22,14 @@ WORKING_DIR=$(mktemp -d veracode_bitcode.XXXXXX)
 cd $WORKING_DIR
 
 # RUN XCODEBUILD
-xcodebuild archive -"${XCODE_TYPE}" "$PROJECT_PATH" -scheme "$SCHEME" -archivePath "$PROJECT" -destination generic/platform=iOS DEBUG_INFORMATION_FORMAT=dwarf-with-dsym ENABLE_BITCODE=YES
+if [ "${IS_RELEASE}" = "release" ]
+then
+    echo "Building as Release"
+    xcodebuild -configuration Release archive -"${XCODE_TYPE}" "$PROJECT_PATH" -scheme "$SCHEME" -archivePath "$PROJECT" -destination generic/platform=iOS DEBUG_INFORMATION_FORMAT=dwarf-with-dsym ENABLE_BITCODE=YES
+else
+    echo "Building as Debug"
+    xcodebuild archive -"${XCODE_TYPE}" "$PROJECT_PATH" -scheme "$SCHEME" -archivePath "$PROJECT" -destination generic/platform=iOS DEBUG_INFORMATION_FORMAT=dwarf-with-dsym ENABLE_BITCODE=YES
+fi
 
 # MOVE APPLICATIONS DIRECTORY OUT OF PRODUCTS AND UP TO PARENT
 mv ${PROJECT}.xcarchive/Products/Applications/ ${PROJECT}.xcarchive/Payload/
